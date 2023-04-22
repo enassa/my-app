01ee9d4af5e5e717f977b6f34943b2e37f296831
nathanie.assan <nathanie.assan@turntabl.io>
2023-04-22 15:07:53 +0000

    added voted object

---
+++ voted.js
@@ -0,0 +1,88 @@
+const electionObjectsample = {
+  voterId: "bc0677de",
+  orgCode: "391970",
+  electionId: "64f682cf24aa0d35",
+  Positions: [
+    {
+      Id: 0,
+      Title: "Portfolio 1",
+      ContestantCount: "",
+      Contestants: [],
+      VoteCount: null,
+      Categories: [],
+      Settings: {
+        Id: 0,
+        title: "single",
+        maxSelection: 1,
+        minSelection: 1,
+        description:
+          "A normal portfolio where users can select only contestant from the list",
+        optional: false,
+      },
+    },
+  ],
+  Title: "My Super Election",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjY0ZjY4MmNmMjRhYTBkMzUiLCJpYXQiOjE2ODIxNTI5NjYsImV4cCI6MTY5MDc5Mjk2Nn0.jOnoKVQGOt5R-g_5KgymVQ-DJqO7oSxb9TRVnUnTDT0",
+  Contestants: [
+    {
+      Id: 0,
+      Info: {
+        Ballot_Number: "1",
+        Name: "Ewudzi Assan",
+        ImageUrl:
+          "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Fdummyprofile.jpg_85bd2927?alt=media&token=9f28eff5-056d-4a46-92dd-85ffa39c8181",
+        ImageInfo:
+          "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Fdummyprofile.jpg_85bd2927?alt=media&token=9f28eff5-056d-4a46-92dd-85ffa39c8181",
+      },
+      Position: "Portfolio 1",
+      PositionId: 0,
+      VotesCount: null,
+      Votes: [],
+    },
+    {
+      Id: 1,
+      Info: {
+        Ballot_Number: "2",
+        Name: "Kusi",
+        ImageUrl:
+          "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Floginpng.png_795d513d?alt=media&token=88b9d038-f01e-437c-a1bc-e56ec3e42ada",
+        ImageInfo:
+          "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Floginpng.png_795d513d?alt=media&token=88b9d038-f01e-437c-a1bc-e56ec3e42ada",
+      },
+      Position: "Portfolio 1",
+      PositionId: 0,
+      VotesCount: null,
+      Votes: [],
+    },
+  ],
+  ContestantDefinition: [
+    {
+      Title: "Name",
+      Id: 0,
+      Type: "Text",
+      Show: true,
+      Invisible: false,
+      fieldDef: "Text",
+    },
+  ],
+  Votes: {
+    0: [
+      {
+        Id: 1,
+        Info: {
+          Ballot_Number: "2",
+          Name: "Kusi",
+          ImageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Floginpng.png_795d513d?alt=media&token=88b9d038-f01e-437c-a1bc-e56ec3e42ada",
+          ImageInfo:
+            "https://firebasestorage.googleapis.com/v0/b/koinovote-361416.appspot.com/o/ACHIMOTA%20SCHOOL_e469b43d%2Floginpng.png_795d513d?alt=media&token=88b9d038-f01e-437c-a1bc-e56ec3e42ada",
+        },
+        Position: "Portfolio 1",
+        PositionId: 0,
+        VotesCount: null,
+        Votes: [],
+      },
+    ],
+  },
+};
